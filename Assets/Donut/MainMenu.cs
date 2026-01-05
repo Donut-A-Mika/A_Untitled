1b4795cd13b93ef3c2e0ca1c318685366f55980a
Donut-A-Mika <Donut11987@gmail.com>
2026-01-05 20:29:47 +0700

    Menu Beta

---
+++ Assets/Donut/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    public void Test()
+    {
+        Debug.Log("CLICK!");
+    }
+}
