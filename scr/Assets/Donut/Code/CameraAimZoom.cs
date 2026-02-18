671a4731a869dcc71ed7281c9861c4bba9d54177
MGT ganm <106914628+Mgtganm@users.noreply.github.com>
2026-02-19 00:30:04 +0700

    update

---
+++ scr/Assets/Donut/Code/CameraAimZoom.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraAimZoom : MonoBehaviour
+{
+    public CinemachineCamera cineCam;
+
+    [Header("Zoom Setting")]
+    public float normalFOV = 50f;
+    public float adsFOV = 30f;
+    public float zoomSpeed = 10f;
+
+    void Update()
+    {
+        bool isAiming = Input.GetMouseButton(1); // คลิกขวาค้าง
+
+        float targetFOV = isAiming ? adsFOV : normalFOV;
+
+        var lens = cineCam.Lens;
+        lens.FieldOfView = Mathf.Lerp(
+            lens.FieldOfView,
+            targetFOV,
+            zoomSpeed * Time.deltaTime
+        );
+
+        cineCam.Lens = lens;
+    }
+}
